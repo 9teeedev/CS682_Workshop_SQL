f617e72b71ff012730a6ef143f358f1dcf1b21b6
9teeedev <teezeezax2@gmail.com>
2025-12-02 10:18:51 +0700

    update file

--- test.sql
+++ test.sql
@@ -1,5 +1,21 @@
-SELECT OrderID, OD.ProductID, ProductName, CategoryName, OD.UnitPrice, OD.Quantity, (OD.UnitPrice * OD.Quantity) AS TotalPrice,
-(OD.UnitPrice * OD.Quantity * (1 - OD.Discount)) AS PriceAfterDiscount
-FROM [Order Details] OD INNER JOIN Products P ON OD.ProductID = P.ProductID
-INNER JOIN Categories C ON P.CategoryID = C.CategoryID
-WHERE OrderID = @OrderID;
+/* CREATE PROCEDURE sp_OrderDetails
+AS
+BEGIN
+    SELECT OrderID, OD.ProductID, ProductName, CategoryName, OD.UnitPrice, OD.Quantity, (OD.UnitPrice * OD.Quantity) AS TotalPrice,
+    (OD.UnitPrice * OD.Quantity * (1 - OD.Discount)) AS PriceAfterDiscount
+    FROM [Order Details] OD INNER JOIN Products P ON OD.ProductID = P.ProductID
+    INNER JOIN Categories C ON P.CategoryID = C.CategoryID
+END; */
+
+/* ALTER PROCEDURE sp_OrderDetails
+    @OrderID INT
+AS
+BEGIN
+    SELECT OrderID, OD.ProductID, ProductName, CategoryName, OD.UnitPrice, OD.Quantity, (OD.UnitPrice * OD.Quantity) AS TotalPrice,
+    (OD.UnitPrice * OD.Quantity * (1 - OD.Discount)) AS PriceAfterDiscount
+    FROM [Order Details] OD INNER JOIN Products P ON OD.ProductID = P.ProductID
+    INNER JOIN Categories C ON P.CategoryID = C.CategoryID
+    WHERE OrderID = @OrderID;
+END; */
+
+EXEC sp_OrderDetails 10248;
